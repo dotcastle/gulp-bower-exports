757ffe3e76d0e7dd8a10c8f3bccb99ddaa105325
Ravi Kiran Katha <ravikiran@kaatha.com>
2015-10-04 00:40:08 +0530

    For npm publish

--- vs/post-build.bat
+++ vs/post-build.bat
@@ -2,7 +2,6 @@
 copy /Y "plugin.js" "gulp-bower-exports.js"
 copy /Y "package.json" ".."
 copy /Y "plugin.js" "..\index.js"
-copy /Y "..\test\bower-exports.json" "..\config"
 
 cd ..
 md package 2> nul
